a8c630f5b41d11318add48380f398f64bdff770e
darshit <darshitvvora@gmail.com>
2016-04-24 00:45:08 +0530

    profile window

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,13 @@ $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
     $(".right-header").toggle(900);
 });
+
+
+$('#myModal').on('shown.bs.modal', function () {
+    // do cool stuff here...
+    $(".progress-bar").animate({
+        width: "70%"
+    }, 1000 );
+});
+
+//Populate users in side windows
